48ad3ba441605c03320fec13b3ccc5749adf58d8
Sabot Media <info@sabot.media>
2026-09-03 04:08:57 -0700

    add browser pwa build mode

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'browser-local' ? './' : '/',
   plugins: [react()],
   server: {
     port: 4173,
     host: true,
   },
-})
+}))

0d597d45484f652b1c2964a23352c88c992c7c5c
Sabot Media <info@sabot.media>
2026-09-03 01:53:28 -0700

    expose desktop helpers

---
+++ desktop/preload.mjs
@@ -0,0 +1,11 @@
+import { contextBridge, ipcRenderer } from 'electron'
+
+contextBridge.exposeInMainWorld('sabotDesktop', {
+  isDesktop: true,
+  platform: process.platform,
+  version: process.versions.electron,
+  openExternal: (url) => ipcRenderer.invoke('desktop:open-external', String(url || '')),
+  openDataFolder: () => ipcRenderer.invoke('desktop:open-data-folder'),
+  showPublishOnline: () => ipcRenderer.invoke('desktop:publish-online'),
+  appInfo: () => ipcRenderer.invoke('desktop:app-info'),
+})
